985cd0be08baa25e1d06d5d80cb11e2d8df03903
osama78assi <osama78assi@gmail.com>
2024-02-02 22:43:01 +0200

    deleted:    dist/favicon.42656662.png deleted:    dist/icons.c14567a0.svg deleted:    dist/index.355db5c0.css deleted:    dist/index.355db5c0.css.map deleted:    dist/index.865f19e0.js deleted:    dist/index.865f19e0.js.map deleted:    dist/index.html deleted:    dist/index.runtime.93367265.js deleted:    dist/index.runtime.93367265.js.map deleted:    dist/logo.833dfef5.png modified:   index.html deleted:    src/img/favicon.png deleted:    src/img/icons.svg deleted:    src/img/logo.png deleted:    src/js/config.js deleted:    src/js/controller.js deleted:    src/js/helperFunctions.js deleted:    src/js/model.js deleted:    src/js/views/addRecipeView.js deleted:    src/js/views/bookmarksView.js deleted:    src/js/views/paginationView.js deleted:    src/js/views/recipeView.js deleted:    src/js/views/resultsView.js deleted:    src/js/views/searchView.js deleted:    src/sass/_base.scss deleted:    src/sass/_components.scss deleted:    src/sass/_header.scss deleted:    src/sass/_mixins.scss deleted:    src/sass/_preview.scss deleted:    src/sass/_recipe.scss deleted:    src/sass/_searchResults.scss deleted:    src/sass/_upload.scss deleted:    src/sass/main.scss

--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
-const API_KEY = `a5245092-a6ff-4a06-beb4-af33a86847df`;
-const TIMEOUT_SEC = 15;
-const RECIPE_PER_PAGE = 10;
-
-export { API_URL, API_KEY, TIMEOUT_SEC, RECIPE_PER_PAGE };
